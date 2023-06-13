7bfa3c4edb0d1b1930cebf1fc30dde18b6485193
Tim Hsu <tim37021@gmail.com>
2023-06-13 07:35:11 +0000

    is_a to is_a?

--- lib/openpgp/buffer.rb
+++ lib/openpgp/buffer.rb
@@ -68,7 +68,7 @@ module OpenPGP
     # @return [Buffer]
     # @see    http://tools.ietf.org/html/rfc4880#section-3.1
     def write_number(number, count)
-      if number.is_a(String)
+      if number.is_a?(String)
         number = number.to_i(16)
       end
 
